1fa7e6d22e122631a89f5a6f5f2804788f8c0979
Heemin <viridian135@gmail.com>
2022-08-09 16:29:15 +0900

    Navigation antd 신버전으로 수정

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,28 +1,47 @@
 import React from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { Menu } from "antd";
+import { Menu, Input } from "antd";
+import { HomeOutlined } from "@ant-design/icons";
+
+function getItem(label, key, icon, type) {
+  return {
+    key,
+    icon,
+    label,
+    type,
+  };
+}
+
+const items = [
+  getItem(
+    <Link href="/">
+      <a>Home</a>
+    </Link>,
+    "sub1",
+    <HomeOutlined />
+  ),
+  getItem(
+    <Link href="/profile">
+      <a>Profile</a>
+    </Link>,
+    "sub2",
+    <HomeOutlined />
+  ),
+  getItem(<Input.Search />, "sub3", <HomeOutlined />),
+  getItem(
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>,
+    "sub4",
+    <HomeOutlined />
+  ),
+];
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <Menu mode="horizontal">
-        <Menu.Item>
-          <Link href="/">
-            <a>home</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link href="/profile">
-            <a>profile</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link href="/signup">
-            <a>signup</a>
-          </Link>
-        </Menu.Item>
-      </Menu>
+      <Menu mode="horizontal" items={items} />
       {children}
     </div>
   );
