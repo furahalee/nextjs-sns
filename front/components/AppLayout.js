fba55e22c943d6560c9ebfa12995edacb7a3206a
Heemin <viridian135@gmail.com>
2022-08-09 17:43:18 +0900

    AppyLayout grid 모바일, 데스크탑

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { Menu, Input } from "antd";
-import { HomeOutlined } from "@ant-design/icons";
+import { Menu, Input, Row, Col } from "antd";
+import {
+  HomeOutlined,
+  ProfileOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
 
 function getItem(label, key, icon, type) {
   return {
@@ -26,15 +30,19 @@ const items = [
       <a>Profile</a>
     </Link>,
     "sub2",
-    <HomeOutlined />
+    <ProfileOutlined />
   ),
-  getItem(<Input.Search />, "sub3", <HomeOutlined />),
+  getItem(
+    <Input.Search enterButton style={{ verticalAlign: "middle" }} />,
+    "sub3",
+    null
+  ),
   getItem(
     <Link href="/signup">
       <a>Signup</a>
     </Link>,
     "sub4",
-    <HomeOutlined />
+    <UserAddOutlined />
   ),
 ];
 
@@ -42,7 +50,23 @@ const AppLayout = ({ children }) => {
   return (
     <div>
       <Menu mode="horizontal" items={items} />
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://github.com/furahalee"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Made by Furahalee
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
