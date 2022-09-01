d3564ee84e863c219f20f2b527681dc873c4ae5d
Heemin <viridian135@gmail.com>
2022-09-01 14:38:52 +0900

    profile 작성

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,14 +1,31 @@
 import React from "react";
-import AppLayout from "../components/AppLayout";
 import Head from "next/head";
+import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
+  const followerList = [
+    { nickname: "베토벤" },
+    { nickname: "퀴리" },
+    { nickname: "쇼팽" },
+  ];
+  const followingList = [
+    { nickname: "베토벤" },
+    { nickname: "퀴리" },
+    { nickname: "쇼팽" },
+  ];
+
   return (
     <>
       <Head>
         <title>내 프로필\nextjs sns</title>
       </Head>
-      <AppLayout>프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followerList} />
+        <FollowList header="팔로워 목록" data={followingList} />
+      </AppLayout>
     </>
   );
 };
